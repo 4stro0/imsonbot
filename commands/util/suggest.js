0ac81f913064983b2a6f15aadf61921a9e805500
naughtyboysd <100404622+naughtyboysd@users.noreply.github.com>
2022-04-07 05:23:51 +0800

    s

---
+++ commands/util/suggest.js
@@ -0,0 +1,25 @@
+const { Message, Client , MessageEmbed , MessageActionRow ,  MessageButton } = require('discord.js')
+module.exports = {
+name: 'suggest', 
+description : 'Suggestikan Ide Gila Mu!!',
+/**
+*
+* @param {Client} client
+* @param {Message} message
+* @param {String[]} args
+*/
+run: async (client, message, args) => {
+    const channel = client.channels.cache.get("961374623155507320")
+    const suggest = args.join("")
+    if(!suggest) return message.reply('Masukan Suggesti Anda')
+
+     const embed = new Discord.MessageEmbed()
+        .setTitle(`From ${message.author.tag}`)
+        .setDescription(suggest)
+    
+        const msw = await channel.send(embed)
+
+        msw.react('👍')
+        msw.react('👎')
+}
+}
