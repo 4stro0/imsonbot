458f09a63f4babcbec76982485d0ca610e99d5db
naughtyboysd <100404622+naughtyboysd@users.noreply.github.com>
2022-04-07 18:33:47 +0800

    s

--- events/guildMemberDelete.js
+++ events/guildMemberDelete.js
@@ -2,5 +2,5 @@ const client = require('../index')
 
 client.on('guildMemberRemove', async(message) => {
     const goodbye = client.channels.cache.get('960111692858155058')
-    goodbye.send(`Yah Kok Keluar <@${message.user.tag}> `)
+    goodbye.send(`Yah Kok Keluar **${message.user.tag}** `)
 })
