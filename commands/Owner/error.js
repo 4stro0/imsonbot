7ad731abcc6befd59fcabe43bf49389cbb1f9d55
naughtyboysd <100404622+naughtyboysd@users.noreply.github.com>
2022-04-17 06:00:02 +0800

    s

---
+++ commands/Owner/error.js
@@ -0,0 +1,14 @@
+const { Message, Client , MessageEmbed , MessageActionRow ,  MessageButton } = require('discord.js')
+module.exports = {
+name: 'error', 
+description : 'test error',
+/**
+*
+* @param {Client} client
+* @param {Message} message
+* @param {String[]} args
+*/
+run: async (client, message, args) => {
+    message.rep
+}
+}
