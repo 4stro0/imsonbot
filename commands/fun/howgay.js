960b0ae1a906a3701b0f6c00c410600d467fa420
naughtyboysd <100404622+naughtyboysd@users.noreply.github.com>
2022-04-16 16:47:57 +0800

    s

--- commands/fun/howgay.js
+++ commands/fun/howgay.js
@@ -1,7 +1,7 @@
 const { Message, Client , MessageEmbed , MessageActionRow ,  MessageButton } = require('discord.js')
 module.exports = {
-name: 'howgay', 
-description : 'melihat seberapa gay',
+name: 'how', 
+description : 'Seberapa?',
 /**
 *
 * @param {Client} client
@@ -9,12 +9,10 @@ description : 'melihat seberapa gay',
 * @param {String[]} args
 */
 run: async (client, message, args) => {
+    const apa = args.join(" ")
+    if(!apa) return message.reply('+how [hal yang diperhitungkan] [siapa]')
     const mention = message.mentions.members.first() || message.author
     const gay = Math.floor(Math.random() * 100)
-    if(gay === 100) return message.reply(`${mention} Dia ${gay}% Gay , Anjir Jauhin Cuk Gay Kali Dia`)
-    if(gay < 100 ) return message.reply(`${mention} Dia ${gay}% Gay , Mengandung Unsur Gay Mnding Jauhin Dah`)
-    if(gay < 50) return message.reply(`${mention} Dia ${gay}% Gay , Sedikit Gay , Kurangin Nafus Mu Kawan`)
-    if(gay < 10) return message.reply(`${mention} Dia ${gay}% Gay  , Jangan Sampe Dia Tercemari Lebih Dalam`)
-    if(gay === 0) return message.reply(`Syukurlah , ${mention} Dia Tidak Gay Sama Sekali`)
+    message.reply(`${mention} Dia ${gay} ${apa}`)
 }
 }
